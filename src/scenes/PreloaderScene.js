749502d0eab2cc07976bf2ba46aa860f7bff3fc0
rahsapr <rahsapr@amazon.co.uk>
2025-07-30 22:53:53 -0700

    Update PreloaderScene.js

--- src/scenes/PreloaderScene.js
+++ src/scenes/PreloaderScene.js
@@ -1,32 +1,35 @@
+// src/scenes/PreloaderScene.js
 export default class PreloaderScene extends Phaser.Scene {
     constructor() {
         super({ key: 'PreloaderScene' });
     }
 
     preload() {
-        // Display a loading bar
+        // --- Loading Bar, no changes here ---
         const progressBar = this.add.graphics();
         const progressBox = this.add.graphics();
         progressBox.fillStyle(0x222222, 0.8);
         progressBox.fillRect(240, 270, 320, 50);
-
         this.load.on('progress', (value) => {
             progressBar.clear();
             progressBar.fillStyle(0xffffff, 1);
             progressBar.fillRect(250, 280, 300 * value, 30);
         });
-
         this.load.on('complete', () => {
             progressBar.destroy();
             progressBox.destroy();
-            this.scene.start('GameScene'); // Start the main game scene
+            this.scene.start('GameScene');
         });
 
-        // Load our assets
-        this.load.image('tiles', 'assets/images/tilesets/grove-tileset.png');
-        this.load.spritesheet('player', 'assets/images/sprites/glimmer-sprite.png', {
-            frameWidth: 32,
-            frameHeight: 32
+        // --- Load OUR NEW Assets ---
+        // Load the world tileset image
+        this.load.image('tiles', 'assets/images/tilesets/tiles.png');
+
+        // Load the character spritesheet, specifying its properties
+        this.load.spritesheet('player', 'assets/images/sprites/characters.png', {
+            frameWidth: 24,
+            frameHeight: 24,
+            spacing: 1 // Crucial: The 1px space between each frame
         });
     }
 }
