302176497a3dad742b3f78c62adfa439216b0aba
rahsapr <rahsapr@amazon.co.uk>
2025-07-30 22:55:45 -0700

    Update Player.js

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,63 +1,66 @@
+// src/classes/Player.js
 export default class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, frame) {
-        super(scene, x, y, texture, frame);
+    constructor(scene, x, y) {
+        // Use the 'player' texture key, start with frame 0 (green character idle)
+        super(scene, x, y, 'player', 0);
 
-        // Add the player to the existing scene
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        // Player physics properties
         this.setBounce(0.1);
         this.setCollideWorldBounds(true);
-        this.body.setSize(20, 32); // Adjust hitbox to be tighter
 
-        // Cursors for input
+        // Adjust hitbox to be tighter for the 24x24 sprite
+        this.body.setSize(16, 22).setOffset(4, 2);
+
         this.cursors = scene.input.keyboard.createCursorKeys();
-        
-        // Initialize animations
         this.initAnims();
     }
 
     initAnims() {
-        // Create animations if they don't already exist
+        // Animations for the green character
         if (!this.scene.anims.exists('idle')) {
             this.scene.anims.create({
                 key: 'idle',
-                frames: this.scene.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-                frameRate: 8,
-                repeat: -1
+                frames: [{ key: 'player', frame: 0 }], // Frame 0 is idle
+                frameRate: 1,
             });
         }
         if (!this.scene.anims.exists('run')) {
             this.scene.anims.create({
                 key: 'run',
-                frames: this.scene.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
-                frameRate: 10,
+                // Frame 1 is the running frame
+                frames: this.scene.anims.generateFrameNumbers('player', { frames: [0, 1] }),
+                frameRate: 8,
                 repeat: -1
             });
         }
     }
 
     update() {
-        // Player Movement Logic
-        const speed = 200;
+        const speed = 250;
+        const onGround = this.body.blocked.down;
 
         if (this.cursors.left.isDown) {
             this.setVelocityX(-speed);
-            this.setFlipX(true); // Flip sprite to the left
-            this.anims.play('run', true);
+            this.setFlipX(true);
+            if (onGround) this.anims.play('run', true);
         } else if (this.cursors.right.isDown) {
             this.setVelocityX(speed);
-            this.setFlipX(false); // Normal sprite orientation
-            this.anims.play('run', true);
+            this.setFlipX(false);
+            if (onGround) this.anims.play('run', true);
         } else {
             this.setVelocityX(0);
-            this.anims.play('idle', true);
+            if (onGround) this.anims.play('idle', true);
         }
 
-        // Jumping
-        if (this.cursors.up.isDown && this.body.blocked.down) {
-            this.setVelocityY(-380);
+        if (this.cursors.up.isDown && onGround) {
+            this.setVelocityY(-400);
+        }
+        
+        // Show a "jumping" frame if in the air
+        if (!onGround) {
+            this.setTexture('player', 1);
         }
     }
 }
